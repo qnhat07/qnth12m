98e836d01993118a897cba26dbcf692b87b4fec8
Nhật <95899527+qnhat07@users.noreply.github.com>
2025-02-21 10:16:45 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+ let u= username.value
+ let p= pw.value
+ if (u=="nigga" && p=="123")
+
+ {
+   window.open("home.html")
+   document.getElementById(compiler).style.display="block"
+}
